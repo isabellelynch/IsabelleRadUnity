d651412f648b28ae87edd4a21a7404fb828fc07b
STUDENT Isabelle Lynch <Isabelle.Lynch@students.ittralee.ie>
2024-10-17 10:52:36 +0100

    Signed-off-by: STUDENT Isabelle Lynch <Isabelle.Lynch@students.ittralee.ie>

--- radGames/Assets/myHeroScript.cs
+++ radGames/Assets/myHeroScript.cs
@@ -4,15 +4,48 @@ using UnityEngine;
 
 public class myHeroScript : MonoBehaviour
 {
+    float runningSpeed = 3f;
+    float turningSpeed = 45f;
     // Start is called before the first frame update
     void Start()
     {
-        transform.position += new Vector3(0,1,0)*Time.deltaTime;   
+        transform.position += new Vector3(0, 1, 0) * Time.deltaTime;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.W))
+        { transform.position += runningSpeed*transform.forward * Time.deltaTime; 
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        { transform.position -= runningSpeed*transform.forward * Time.deltaTime; 
+        }
+
+        if (Input.GetKey(KeyCode.E))
+        {
+            transform.position += runningSpeed * transform.right * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position -= runningSpeed * transform.right * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            transform.position += runningSpeed * transform.up * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.Backspace))
+        {
+            transform.position -= runningSpeed * transform.up * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        { transform.Rotate(transform.up, turningSpeed* Time.deltaTime ); 
+        }
     }
+
 }
